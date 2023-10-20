f847ce443eb2910750bdbefe14eb1c95f697ac79
NguyenLong <longsky0912624119@gmail.com>
2023-10-20 11:11:39 +0700

    Hoan thien layout thu 3, tiep tuc lam layout4...

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Layout1 from './src/components/layout1';
 import Layout2 from './src/components/layout2';
+import Layout3 from './src/components/layout3';
 
 
 export default function App() {
   return (
     //  <Layout1/>
-      <Layout2/>
-    // <Layout3/>
+    //<Layout2/>
+     <Layout3/>
     //  <Layout4/> 
   );
 }
