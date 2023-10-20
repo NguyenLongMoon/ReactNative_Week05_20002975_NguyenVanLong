06a5b2687de0588f4dee255f86973f06930d44a4
NguyenLong <longsky0912624119@gmail.com>
2023-10-20 11:00:26 +0700

    Da hoan thien layout 2

--- App.js
+++ App.js
@@ -2,12 +2,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Layout1 from './src/components/layout1';
+import Layout2 from './src/components/layout2';
 
 
 export default function App() {
   return (
-     <Layout1/>
-    //  <Layout2/>
+    //  <Layout1/>
+      <Layout2/>
     // <Layout3/>
     //  <Layout4/> 
   );
