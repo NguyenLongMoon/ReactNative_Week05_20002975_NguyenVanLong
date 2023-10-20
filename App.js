b22bcff986dedcf680be861441d3719ac89d0c2c
NguyenLong <longsky0912624119@gmail.com>
2023-10-20 10:52:49 +0700

    Hoan thanh layout 1

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Layout1 from './src/components/layout1';
+
+
+export default function App() {
+  return (
+     <Layout1/>
+    //  <Layout2/>
+    // <Layout3/>
+    //  <Layout4/> 
+  );
+}
